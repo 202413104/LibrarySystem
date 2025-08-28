a60d5fea3bbe0c66f711536eb7de668c039ba6aa
202413104 <suminhyung1227@naver.com>
2025-08-28 08:24:48 +0000

    완성

--- src/service/LibraryTest.java
+++ src/service/LibraryTest.java
@@ -5,6 +5,20 @@ package service;
 public class LibraryTest {
 
     public static void main(String[] args) {
+        LibraryItem[]items={
+            new Book("객체지향 프로그래밍 입문"),
+            new DVD("인셉션")
+
+        };
+        (LibraryItem item:items);
+            item item:items){
+            item.displayInfo();
+            item.borrow
+            item.returnItem();
+            System.out.println();
+        })
+
+
 
     }
 }
