00406c5faf7e67dccdfe9f9d264c11b234ab242e
MinseoKim01 <202513341@pukyong.ac.kr>
2025-08-28 17:28:02 +0900

    Libraryitem.java 작성
    도서관 책의 기본 뼈대 정의, 실제 동작은 Book 자식 클래스가 구현하게 해주는 베이스 코드 작성

--- src/model/Libraryitem.java
+++ src/model/Libraryitem.java
@@ -3,4 +3,7 @@ package model;
 
 //MINSEO
 abstract  class Libraryitem implements Borrowable{
+    protected String title;
+    public LibraryItem(String title) { this.title = title; }
+    public abstract void displayInfo();
 }
